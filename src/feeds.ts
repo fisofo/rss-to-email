d2a3a8b2bd030494fce9f6aed9e3a73dc4455c6b
fisofo <fisofo@users.noreply.github.com>
2023-07-04 18:48:27 -0500

    Update feeds.ts

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,10 +1,31 @@
 export const feeds = [
-  // Daring Fireball
-  'https://daringfireball.net/feeds/main',
+  // Penny Arcade
+  'https://www.penny-arcade.com/feed',
 
-  // JakeArchibald.com
-  'https://jakearchibald.com/posts.rss',
+  // Saturday Morning Breakfast Cereal (updated daily)
+  'http://www.smbc-comics.com/rss.php',
 
-  // Appjeniksaan
-  'https://appjeniksaan.nl/feed.xml',
+  // Existential Comics
+  'http://existentialcomics.com/rss.xml',
+
+  // xkcd.com
+  'http://xkcd.com/rss.xml',
+
+  // Wondermark
+  'http://feeds.feedburner.com/wondermark',
+
+  // The Oatmeal - Comics, Quizzes, & Stories
+  'http://feeds.feedburner.com/oatmealfeed',
+
+  // Dilbert Daily Strip
+  'http://feeds.feedburner.com/DilbertDailyStrip',
+
+  // FoxTrot | FoxTrot
+  'http://www.foxtrot.com/feed/',
+
+  // Dinosaur Comics!
+  'https://www.qwantz.com/rssfeed.php',
+
+  // Krebs on Security
+  'https://krebsonsecurity.com/feed',
 ]
